2f8f524f6258b27c7ba93c905ab2619d480bac2f
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2025-01-05 14:09:16 +0100

    Added corsheader to allow communication between front and backend

--- violetteStores/settings.py
+++ violetteStores/settings.py
@@ -40,16 +40,23 @@ INSTALLED_APPS = [
     'core',
     'shop',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# DJANGO CORS
+CORS_ALLOWED_ORIGINS = [
+"http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'violetteStores.urls'
